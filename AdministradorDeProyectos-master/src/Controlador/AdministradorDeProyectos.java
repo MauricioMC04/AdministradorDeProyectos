7aa3c32450aac2f973993604c40264c39836f3da
Luis <Luis@10.18.2.89>
2017-10-18 17:21:52 -0600

    Creación de Clase modelo

--- AdministradorDeProyectos-master/src/Controlador/AdministradorDeProyectos.java
+++ AdministradorDeProyectos-master/src/Controlador/AdministradorDeProyectos.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controlador;
 
 import Modelo.Conexion;
 import java.sql.Connection;
 
-/**
- *
- * @author Josue
- */
+
 public class AdministradorDeProyectos {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Conexion conec = new Conexion();
         Connection c = conec.conexion();
