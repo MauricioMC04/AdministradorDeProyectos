1728a2a51fa0fab79d52308886ca822c9ff42bd7
Luis <Luis@10.18.2.89>
2017-10-19 10:10:04 -0600

    Creacion de metodologin y vista login

--- src/Modelo/Conexion.java
+++ src/Modelo/Conexion.java
@@ -14,7 +14,7 @@ public class Conexion {
         try {
             conexion = null;
             Class.forName("com.mysql.jdbc.Driver");
-            conexion = DriverManager.getConnection("jdbc:mysql://127.0.0.1/conexion", "Proyecto", "123");
+            conexion = DriverManager.getConnection("jdbc:mysql://sql10.freemysqlhosting.net/sql10199786", "sql10199786", "ePKENjFQXk");
             System.out.println("conexion establecida");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("error de conexion");
