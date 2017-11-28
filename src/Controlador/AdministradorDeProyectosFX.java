0edf4f9c708dfbd2be4163ed95d1a820cf567b77
Josue <Josue@192.168.1.2>
2017-11-28 17:22:30 -0600

    Editar Departamentos

--- src/Controlador/AdministradorDeProyectosFX.java
+++ src/Controlador/AdministradorDeProyectosFX.java
@@ -95,6 +95,8 @@ private void comparar(){
     }
 
     public static void main(String[] args) {
+        String userDir = System.getProperty("user.dir");
+        System.out.println(userDir);
         launch(args);
     }
     
